fix length rabi to roughly work now that we only use pihalf in clifford averager; fitting code (not super stable) for amp rabi eg-gf, a bit of debug on picking a freq to plot on res spec power sweep, minor updates on full_mux readout to plotIQ

experiments/clifford_averager_program.py
@@ -1,6 +1,5 @@
 import logging
 
-import experiments.fitting as fitter
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
@@ -8,6 +7,8 @@ from qick import *
 from qick.helpers import ch2list
 from slab import AttrDict, Experiment
 from tqdm import tqdm_notebook as tqdm
+
+import experiments.fitting as fitter
 
 logger = logging.getLogger("qick.qick_asm")
 logger.setLevel(logging.ERROR)
@@ -767,6 +768,11 @@ class CliffordAveragerProgram(AveragerProgram):
             times_us = np.linspace(0, tot_length_us, int(tot_length_us / dt_us))
             pulse_I_shapes = np.ones((len(mux_freqs), len(times_us)))
             pulse_Q_shapes = np.zeros((len(mux_freqs), len(times_us)))
+            
+        print("times_us", times_us)
+        print("pulse_I", pulse_I_shapes)
+        print("pulse_Q", pulse_Q_shapes)
+        
 
         tot_I_vs_us = np.zeros_like(times_us)
         tot_Q_vs_us = np.zeros_like(times_us)
@@ -850,7 +856,7 @@ class CliffordAveragerProgram(AveragerProgram):
             assert mask is not None
             assert mux_freqs is not None
             assert mixer_freq is not None
-            assert lengths is not None
+            # assert lengths is not None
             tot_I_vs_us, tot_Q_vs_us, times_us = self.setup_full_mux_pulse(
                 mask=mask,
                 mux_freqs=mux_freqs,
@@ -1200,6 +1206,10 @@ class CliffordAveragerProgram(AveragerProgram):
             mixer_freqs = np.array(self.cfg.hw.soc.dacs.readout.mixer_freq)
             assert np.all(mixer_freqs == mixer_freqs[0])
             mixer_freq = mixer_freqs[0]
+            if 'plot_IQ' in self.cfg.expt:
+                plot_IQ = self.cfg.expt.plot_IQ
+            else:
+                plot_IQ = False
 
             self.setup_fullmux_readout(
                 full_mux_ch=full_mux_ch,
@@ -1212,7 +1222,7 @@ class CliffordAveragerProgram(AveragerProgram):
                 pulse_I_shapes=pulse_I_shapes,
                 pulse_Q_shapes=pulse_Q_shapes,
                 times_us=times_us,
-                plot_IQ=False,
+                plot_IQ=plot_IQ,
             )
 
     def setup_mux_gen_readout(self):
@@ -1312,7 +1322,7 @@ class CliffordAveragerProgram(AveragerProgram):
         for ch in adc_chs:
             chs_to_round.append(self.soccfg["readouts"][ch])
         rounded_mixer_freq = self.roundfreq(orig_mixer_freq, chs_to_round)
-        rounded_mux_freqs = np.array([self.roundfreq(f, chs_to_round) for f in mux_freqs])
+        rounded_mux_freqs = np.array([np.abs(self.roundfreq(f, chs_to_round)) for f in mux_freqs])
         rounded_freqs = rounded_mixer_freq + rounded_mux_freqs
 
         self.res_chs = [full_mux_ch] * self.num_qubits_sample

experiments/single_qubit/resonator_spectroscopy.py
@@ -248,7 +248,7 @@ class ResonatorPowerSweepSpectroscopyExperiment(Experiment):
                 # avgi, avgq = rspec.acquire(self.im[self.cfg.aliases.soc], load_pulses=True, progress=False)
                 # avgi = avgi[0][0]
                 # avgq = avgq[0][0]
-                
+
                 amp = np.average(np.abs((datai + 1j * dataq)))  # Calculating the magnitude
                 phase = np.average(np.angle(datai + 1j * dataq))  # Calculating the phase
                 # amp = np.abs(avgi + 1j * avgq)  # Calculating the magnitude
@@ -337,12 +337,11 @@ class ResonatorPowerSweepSpectroscopyExperiment(Experiment):
 
         if select is not None:
             fig, ax = plt.subplots()
-            for s in select:
-                y_closest_i = np.argmin(abs(y_sweep - s))
-                y_closest = y_sweep[y_closest_i]
-                y_plot = data["amps"][y_closest_i, :]/np.max(data["amps"][y_closest_i, :])
-                print("plotting at gain", y_closest, "index", y_closest_i)
-                ax.plot(x_sweep, y_plot, "o-", label=f"Gain {y_closest}")
+            y_closest_i = np.argmin(abs(y_sweep - select))
+            y_closest = y_sweep[y_closest_i]
+            y_plot = data["amps"][y_closest_i, :] / np.max(data["amps"][y_closest_i, :])
+            print("plotting at gain", y_closest, "index", y_closest_i)
+            ax.plot(x_sweep, y_plot, "o-", label=f"Gain {y_closest}")
             ax.legend()
 
     def save_data(self, data=None):
@@ -424,7 +423,6 @@ class ResonatorVoltSweepSpectroscopyExperiment(Experiment):
                 avgq = avgq[0][0]
                 amp = np.abs(avgi + 1j * avgq)  # Calculating the magnitude
                 phase = np.angle(avgi + 1j * avgq)  # Calculating the phase
-        
 
                 data["avgi"][-1].append(avgi)
                 data["avgq"][-1].append(avgq)

experiments/two_qubit/amplitude_rabi_EgGf.py
@@ -1,10 +1,13 @@
-import experiments.fitting as fitter
+from copy import deepcopy
+
 import matplotlib.pyplot as plt
 import numpy as np
 from qick import *
 from qick.helpers import gauss
 from slab import AttrDict, Experiment, dsfit
 from tqdm import tqdm_notebook as tqdm
+
+import experiments.fitting as fitter
 
 
 class AmplitudeRabiEgGfProgram(RAveragerProgram):
@@ -810,10 +813,46 @@ class EgGfFreqGainChevronExperiment(Experiment):
         self.data = data
         return data
 
-    def analyze(self, data=None, fit=True, **kwargs):
+    def analyze(self, data=None, fit=True, fitparams=None):
         if data is None:
             data = self.data
-        pass
+
+        data = deepcopy(data)
+        inner_sweep = data["gainpts"]
+        outer_sweep = data["freqpts"]
+
+        y_sweep = outer_sweep  # index 0
+        x_sweep = inner_sweep  # index 1
+
+        length = self.cfg.expt.pi_EgGf_sigma
+
+        # fitparams = [yscale, freq, phase_deg, y0]
+        # fitparams=[None, 2/x_sweep[-1], None, None]
+        for data_name in ["avgi", "avgq", "amps"]:
+            data.update({f"fit{data_name}": [None] * len(self.cfg.expt.measure_qubits)})
+            data.update({f"fit{data_name}_err": [None] * len(self.cfg.expt.measure_qubits)})
+            data.update({f"data_fit{data_name}": [None] * len(self.cfg.expt.measure_qubits)})
+            for q_index in range(len(self.cfg.expt.measure_qubits)):
+                this_data = data[data_name][q_index]
+
+                fit = [None] * len(x_sweep)
+                fit_err = [None] * len(x_sweep)
+                data_fit = [None] * len(x_sweep)
+
+                for i_gain, gain in enumerate(x_sweep):
+                    p, pCov = fitter.fitrabi_gainslice(
+                        y_sweep, this_data[:, i_gain], length=length, fitparams=fitparams
+                    )
+                    fit[i_gain] = p
+                    fit_err[i_gain] = pCov
+                    data_fit[i_gain] = fitter.rabifunc(y_sweep, length, *p)
+
+                data[f"fit{data_name}"][q_index] = fit
+                data[f"fit{data_name}_err"][q_index] = fit_err
+                data[f"data_fit{data_name}"][q_index] = data_fit
+
+        # Output data: fit{data_name}: (len(measure_qubits), len(x_sweep), len(fitparams))
+        return data
 
     def display(self, data=None, fit=True, plot_freq=None, plot_gain=None, saveplot=False, **kwargs):
         if data is None:
@@ -925,6 +964,42 @@ class EgGfFreqGainChevronExperiment(Experiment):
             plt.savefig(plot_filename, format="png", bbox_inches="tight", transparent=True)
             print("Saved", plot_filename)
 
+        plt.show()
+
+        if not fit:
+            return
+
+        """
+        Plot fit chevron
+        """
+        # Output data: fit{data_name}: (len(measure_qubits), len(x_sweep), len(fitparams))
+        fit_qind = 0 if self.cfg.expt.qubits[0] == self.cfg.expt.qDrive else 1
+        fit_freqs = np.array([f[1] if f is not None else np.nan for f in data["fitamps"][fit_qind]])
+
+        plt.figure(figsize=(9, 7))
+        plt.suptitle(f"Fit Eg-Gf Chevron Frequency vs. Gain (Length {self.cfg.expt.pi_EgGf_sigma} us)")
+
+        plt.subplot(221, title=f"Qubit A ({self.cfg.expt.qubits[0]})", ylabel="Pulse Frequency [MHz]")
+        plt.pcolormesh(x_sweep, y_sweep, data["avgi"][0], cmap="viridis", shading="auto")
+        if fit:
+            plt.plot(x_sweep, fit_freqs, color="k", linestyle="--")
+
+        plt.subplot(223, xlabel="Gain [DAC units]", ylabel="Pulse Frequency [MHz]")
+        plt.pcolormesh(x_sweep, y_sweep, data["avgq"][0], cmap="viridis", shading="auto")
+        if fit:
+            plt.plot(x_sweep, fit_freqs, color="k", linestyle="--")
+
+        plt.subplot(222, title=f"Qubit B ({self.cfg.expt.qubits[1]})")
+        plt.pcolormesh(x_sweep, y_sweep, data["avgi"][1], cmap="viridis", shading="auto")
+        if fit:
+            plt.plot(x_sweep, fit_freqs, color="k", linestyle="--")
+
+        plt.subplot(224, xlabel="Gain [DAC units]")
+        plt.pcolormesh(x_sweep, y_sweep, data["avgq"][1], cmap="viridis", shading="auto")
+        if fit:
+            plt.plot(x_sweep, fit_freqs, color="k", linestyle="--")
+
+        plt.tight_layout()
         plt.show()
 
     def save_data(self, data=None):